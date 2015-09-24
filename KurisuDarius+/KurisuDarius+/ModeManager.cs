744c5302040adc365203e3ac79511194234c0efb
elwindy <ouo85327@kmhow.com>
2015-09-25 01:35:38 +0300

    ks r

--- KurisuDarius+/KurisuDarius+/ModeManager.cs
+++ KurisuDarius+/KurisuDarius+/ModeManager.cs
@@ -7,6 +7,10 @@ using EloBuddy.SDK.Utils;
 
 namespace KurisuDarius_
 {
+    using System.Linq;
+
+    using EloBuddy.SDK;
+
     public static class ModeManager
     {
         private static List<ModeBase> Modes { get; set; }
@@ -57,6 +61,37 @@ namespace KurisuDarius_
                     Logger.Log(LogLevel.Error, "Error executing mode '{0}'\n{1}", mode.GetType().Name, e);
                 }
             });
+
+            if (SpellManager.R.IsReady())
+            {
+                foreach (var unit in HeroManager.Enemies.Where(ene => ene.IsValidTarget(SpellManager.R.Range) && !ene.IsZombie))
+                {
+                    int rr = unit.GetBuffCount("dariushemo") <= 0 ? 0 : unit.GetBuffCount("dariushemo");
+                    if (unit.CountEnemiesInRange(1200) <= 1)
+                    {
+                        if (ObjectManager.Player.Distance(unit.ServerPosition) > 265)
+                        {
+                            if (SpellManager.RDmg(unit, rr) + 0 + SpellManager.Hemorrhage(unit, rr) >= unit.Health)
+                            {
+                                if (!unit.HasBuffOfType(BuffType.Invulnerability) &&
+                                    !unit.HasBuffOfType(BuffType.SpellShield))
+                                {
+                                    SpellManager.R.Cast(unit);
+                                }
+                            }
+                        }
+                    }
+
+                    if (SpellManager.RDmg(unit, rr) + 0 >= unit.Health + SpellManager.Hemorrhage(unit, 1))
+                    {
+                        if (!unit.HasBuffOfType(BuffType.Invulnerability) &&
+                            !unit.HasBuffOfType(BuffType.SpellShield))
+                        {
+                            SpellManager.R.Cast(unit);
+                        }
+                    }
+                }
+            }
         }
     }
 }
