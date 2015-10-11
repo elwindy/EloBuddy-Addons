7c6ce2198d9b63dbc62443dbf0249ede1d84b37f
elwindy <ouo85327@kmhow.com>
2015-10-11 21:22:25 +0300

    new core

--- JayceBuddy/JayceBuddy/Program.cs
+++ JayceBuddy/JayceBuddy/Program.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EloBuddy;
 using EloBuddy.SDK;
 using SharpDX;
 using Color = System.Drawing.Color;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
 
 namespace JayceBuddy
 {
-    using EloBuddy.SDK.Enumerations;
-    using EloBuddy.SDK.Events;
-    using EloBuddy.SDK.Menu;
-    using EloBuddy.SDK.Menu.Values;
-
     class Program
     {
 
